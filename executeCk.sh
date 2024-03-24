fe1758fbcb3e48efed3f3b8fd124f1e4f0755645
Henrique Coelho <h8mendes@gmail.com>
2024-03-24 12:07:39 -0300

    add shell to execute results

--- executeCk.sh
+++ executeCk.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-
 # Verifica se o número de argumentos é válido
-if [ "$#" -ne 1 ]; then
-    echo "Uso: $0 <diretório_do_projeto>"
-    exit 1
-fi
+json_file="C:\Users\h8men\OneDrive\Documentos\Cefet\TCC\result.json"
 
-# Armazena o diretório do projeto passado como argumento
-projeto_dir="$1"
+for row in $(jq -r '.[] | .[2]' "${json_file}"); do
+    echo "URL encontrada: ${row}"
 
-# Executa o comando Java com os parâmetros fornecidos
-java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar "$projeto_dir" true 0 False "C:\Users\h8men\OneDrive\Documentos\Cefet\TCC\metrics"
+    # Armazena a URL do repositório GitHub passada como argumento
+    github_url=$row
+    # Extrai a última parte da URL do GitHub para definir o nome do diretório
+    repo_name=$(echo "$github_url" | awk -F'/' '{print $(NF-0)}' | cut -d'.' -f1)
+
+    echo "Clonando o repositório do GitHub $repo_name"
+    git clone "$github_url" "$repo_name"
+
+    if [ "$?" -ne 0 ]; then
+        echo 'Erro ao clonar o repositório do GitHub. Verifique a URL fornecida.'
+        exit 1
+    fi
+
+    # Executa o comando Java com os parâmetros fornecidos
+    metrics_repo='C:\Users\h8men\OneDrive\Documentos\Cefet\TCC\metrics\'
+    java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar "$repo_name" true 0 False "${metrics_repo}${repo_name}"
+
+
+    echo "Deletando o diretório original: $repo_name"
+    rm -rf $repo_name
+done
